b8b6688ddfc14f98ad8b576476d893c940a18129
mhmd fudhail <69971661+mhdfudhail@users.noreply.github.com>
2023-04-21 15:26:50 +0530

    Add files via upload

---
+++ reciever_e5_pico_uart/serialCollector_1.py
@@ -0,0 +1,29 @@
+import serial
+from time import sleep
+
+def collectData():
+    serialData = serial.Serial('COM8', 9600, timeout = 1.0)
+    sleep(2)
+
+    serialData.reset_input_buffer()
+    print("----collecting----")
+    while True:
+        sleep(0.01)    
+        if serialData.in_waiting >0:
+            myData = serialData.readline().decode('utf-8').rstrip()
+            # print(myData)
+            #serialData = "10.029137,76.328377,1"
+            data = myData.split(",")
+            receiCord = [10.029576,76.328476]
+            if len(data)>0:
+                # print(data)
+                print("----collected----")
+                break
+    return data,receiCord
+
+            
+
+if __name__ == '__main__':
+    value = collectData()
+    print(value)
+
